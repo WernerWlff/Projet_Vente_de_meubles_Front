efadc0c20f4721658f65b225c937c28b52ba2dc8
Werner Wolff <werner.wolff1109@gmail.com>
2025-11-17 10:59:00 +0100

    add: Jwt-Decode
    add: All the services files are created

---
+++ Furniture_Store/src/app/services/auth.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+
+export interface JwtPayload{
+  sub : string,
+  iat : number,
+  exp : number
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = "/api";
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, {email, password});
+  }
+
+  setToken(token: string){
+    return localStorage.setItem('Token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('Token');
+  }
+
+  clearToken() {
+    localStorage.removeItem('Token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  logout() {
+    this.clearToken();
+  }
+
+  getUserInfo(): JwtPayload | null {
+    const token = this.getToken();
+    if(!token) {
+      return null
+    }
+    try{
+      return jwtDecode<JwtPayload>(token);
+    }catch(error){
+      console.error("Erreur lors du décodage du token :", error);
+      return null;
+    }
+  }
+
+  // get Username / email
+  getUsername(): string | null {
+    const userInfo = this.getUserInfo();
+    return userInfo ? userInfo.sub : null;
+  }
+}
