355cff2fd7285a056cb80072db2263643b46e98c
Werner Wolff <werner.wolff1109@gmail.com>
2025-11-21 12:21:15 +0100

    add: Zone.js to get a better user interface in case of changes
    add: components to the login page / the front-page / users-list ( admin only )
    
    add: Modal to the users-list page to edit them
    
    need: Modal to create new user in the admin page
    
    add: Login-page is now functionning, still need some css to make it user friendly

---
+++ Furniture_Store/src/app/components/users-list/users-list-modal/users-list-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-list-modal',
+  imports: [],
+  templateUrl: './users-list-modal.html',
+  styleUrl: './users-list-modal.css',
+})
+export class UsersListModal {
+
+}
