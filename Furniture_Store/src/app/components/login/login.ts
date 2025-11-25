7c4eaf86f2737d2666a103ce92751dd0e38cedb8
Werner Wolff <werner.wolff1109@gmail.com>
2025-11-25 14:26:39 +0100

    add: RouterModule to the Login to redirect to the frontPage when we are connected
    fix: html of the login page, it also contain the navbar made for the frontPage

--- Furniture_Store/src/app/components/login/login.ts
+++ Furniture_Store/src/app/components/login/login.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css'],
 })
